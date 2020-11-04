7b58fadc68fad2b00f175a969f2944c04cb62e2e
Christian Otu <christianotu.d@gmail.com>
2020-11-04 21:25:46 +0100

    Added State Provider Context

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { StateProvider, reducer } from "./state";
 
 render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StateProvider reducer={reducer}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </StateProvider>,
   document.querySelector("#root")
 );
