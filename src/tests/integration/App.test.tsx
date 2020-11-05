4c987af0756789afd8d422c569ecb7ecf233817f
Christian Otu <christianotu.d@gmail.com>
2020-11-05 13:57:24 +0100

    Added context state provider

--- src/tests/integration/App.test.tsx
+++ src/tests/integration/App.test.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { render, screen, cleanup } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
+import { StateProvider, reducer } from "../../state";
 import App from "../../App";
 
-afterAll(() => {
-  cleanup();
-})
+beforeEach(() => {
+  render(
+    <StateProvider reducer={reducer}>
+      <App />
+    </StateProvider>)
+});
 
 describe("App", () => {
   test('renders App', () => {
-    render(<App />);
-
     expect(screen.getAllByText(/count: 0/i)).toHaveLength(2);
     expect(screen.getByRole("heading", { name: /shared count/i })).toBeInTheDocument();
     expect(screen.getByRole("button", { name: "+" })).toBeInTheDocument();
@@ -18,24 +20,23 @@ describe("App", () => {
   });
 
   test("increases, decreases, and resets count", () => {
-    render(<App />);
-    
+
     const count = screen.getAllByRole("heading");
 
     expect(count[0]).toBeInTheDocument();
     expect(screen.getByText(/^shared/i)).toBeInTheDocument();
 
-    userEvent.dblClick(screen.getByRole("button", { name: "+"}));
+    userEvent.dblClick(screen.getByRole("button", { name: "+" }));
 
     expect(count[0]).toBeInTheDocument();
     expect(screen.getByText(/^shared/i)).toBeInTheDocument();
 
-    userEvent.click(screen.getByRole("button", { name: "-"}));
+    userEvent.click(screen.getByRole("button", { name: "-" }));
 
     expect(count[0]).toBeInTheDocument();
     expect(screen.getByText(/^shared/i)).toBeInTheDocument();
 
-    userEvent.click(screen.getByRole("button", { name: /reset count/i}));
+    userEvent.click(screen.getByRole("button", { name: /reset count/i }));
 
     expect(count[0]).toBeInTheDocument();
     expect(screen.getByText(/^shared/i)).toBeInTheDocument();
